9cb40d53960e6f87810e03579ee4c1bbf6061963
ylu <123523291+ylu8888@users.noreply.github.com>
2023-09-12 00:04:04 -0400

    Update validargs.c

--- validargs.c
+++ validargs.c
@@ -22,32 +22,29 @@
 int validargs(int argc, char **argv)
 {  //  bin/philo -h -n -o <name>
 
-    argv[0] = -h
-    arvg[1] = -n
-
     int flagH = 0;
     int flagM = 0;
     int flagN = 0;
     int flagO = 0;
     int invalid = 0;
 
-    int counter = 0; //just use counter to check error conditions, don't need to check order of arg strings
-   
+    int counter = 0; //just use counter to check error conditions
+    //don't need to check order of arg strings
+
     //double for loop
     // one iterates through arg array
     // second iterates through char array3
 
-    
     for(int i = 0; i < argc; i++){
          char *char1 = argv;   //iterate through argument array
          char *char2 = *char1; //iterate through arg string
 
         //while the second pointer iterates until reaching null terminator
         while(*char2 != '\0') {
-            
+
             if(*char2 == "-"){
                char2++;
-            
+
                 if(*char2 == "h"){
                     flagH = 1;
                 }
@@ -60,7 +57,7 @@ int validargs(int argc, char **argv)
                 else if(*char2 == "o"){
                     flagO = 1;
                 }
-                
+
             }
             char2++;
         }
@@ -68,29 +65,27 @@ int validargs(int argc, char **argv)
         char1++;
     }
 
-            //if h ignore 
+            //if h ignore
             // m is present if n is also present return failure
             //if m is present and o is present failure
             // if n is present o must be present
             // o is present then name must be present
             // check the order : o must be after n
-            // failure if -x -z -y etc a flag that doesnt exist 
+            // failure if -x -z -y etc a flag that doesnt exist
 
 
             //change the bits in global option before returning 0 or 1
             if(flagM == 1 && flagN == 1){
                 return -1;
             }
-            
 
-            
             if(invalid == 1){
                 return -1;
             }
             else{
                 return 0;
             }
-                
-   
+
+
     abort();
 }
