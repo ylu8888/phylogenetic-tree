48358f8b4fcbf61f1b1104faef63e39efed7b8f8
ylu <123523291+ylu8888@users.noreply.github.com>
2023-09-11 20:58:20 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "global.h"
+#include "debug.h"
+
+int main(int argc, char **argv)
+{
+    if(validargs(argc, argv))
+        USAGE(*argv, EXIT_FAILURE);
+    if(global_options == HELP_OPTION)
+        USAGE(*argv, EXIT_SUCCESS);
+    // TO BE IMPLEMENTED
+    return EXIT_FAILURE; 
+}
+
+/*
+ * Just a reminder: All non-main functions should
+ * be in another file not named main.c
+ */
